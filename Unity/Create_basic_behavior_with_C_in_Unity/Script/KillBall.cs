754503252525b72922d03fc28eb85cf3e43a59a5
Unimauo <unimauro@gmail.com>
2020-06-19 06:35:37 -0500

    Unity Project

---
+++ Unity/Create_basic_behavior_with_C_in_Unity/Script/KillBall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBall : MonoBehaviour
+{
+	private void OnMouseDown()
+	{
+		Destroy(gameObject);
+	}
+
+}
