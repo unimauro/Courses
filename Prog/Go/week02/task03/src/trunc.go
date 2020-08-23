92178ab6bbfb08f8f58826fc7356e9109d8f32e0
Unimauo <unimauro@gmail.com>
2020-08-23 16:00:39 -0500

    Enviando

---
+++ Prog/Go/week02/task03/src/trunc.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var f float64 = 2.55
+	fmt.Scanf("Write the float number: %d", &f)
+	fmt.Println("the truncate number is: \n")
+	fmt.Println(int(f))
+}
+
+
