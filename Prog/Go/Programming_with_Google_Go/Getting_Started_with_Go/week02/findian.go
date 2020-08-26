1409e06f02a8ba8f71b984657a3d1ba2984b6f89
Unimauo <unimauro@gmail.com>
2020-08-26 11:03:09 -0500

    Enviando

--- Prog/Go/Programming_with_Google_Go/Getting_Started_with_Go/week02/findian.go
+++ Prog/Go/Programming_with_Google_Go/Getting_Started_with_Go/week02/findian.go
@@ -9,7 +9,7 @@ import(
 func main(){
 	var s string 
 	fmt.Println("Please write the variable:\n")
-	fmt.Scanfln(&s)
+	fmt.Scanln(&s)
 
 	if strings.Contains(s,"a") && (s[0]=='i' || s[0]=='I') && (s[len(s)-1]=='n' || s[len(s)-1]=='N') {
 		fmt.Print("Found!")
