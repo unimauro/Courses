56770e37e178c514c3d0c51df35848b52fbbf4c2
Unimauo <unimauro@gmail.com>
2020-08-29 23:52:11 -0500

    Architecture  Patterns With Python

---
+++ Prog/Python/Architecture_Patterns_with_Python/model.py
@@ -0,0 +1,18 @@
+@dataclass(frozen=True)  
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(
+        self, ref: str, sku: str, qty: int, eta: Optional[date]  
+    ):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
+
+    def allocate(self, line: OrderLine):
+        self.available_quantity -= line.qty
