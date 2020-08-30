8b04d8908ac0641101343dcde224edaf6328202d
Unimauo <unimauro@gmail.com>
2020-08-29 23:55:50 -0500

    Architecture  Patterns With Python

--- Prog/Python/Architecture_Patterns_with_Python/test_batches.py
+++ Prog/Python/Architecture_Patterns_with_Python/test_batches.py
@@ -28,4 +28,9 @@ def test_can_allocate_if_available_equal_to_required():
 def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
-    assert batch.can_allocate(different_sku_line) is False    
+    assert batch.can_allocate(different_sku_line) is False 
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
