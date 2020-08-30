1ee8b14eba5f475c9d4e711f8fb7e0c82c1d5262
Unimauo <unimauro@gmail.com>
2020-08-30 18:33:25 -0500

    Architecture

---
+++ Prog/Python/Architecture_Patterns_with_Python/OrderidSku.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import NamedTuple
+from collections import namedtuple
+
+@dataclass(frozen=True)
+class Name:
+    first_name: str
+    surname: str
+
+class Money(NamedTuple):
+    currency: str
+    value: int
+
+Line = namedtuple('Line', ['sku', 'qty'])
+
+def test_equality():
+    assert Money('gbp', 10) == Money('gbp', 10)
+    assert Name('Harry', 'Percival') != Name('Bob', 'Gregory')
+    assert Line('RED-CHAIR', 5) == Line('RED-CHAIR', 5)    
