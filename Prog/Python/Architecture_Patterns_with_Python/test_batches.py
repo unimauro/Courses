dcd7b9339c78e230395e2d19e705239c22867ebf
Unimauo <unimauro@gmail.com>
2020-08-29 23:57:05 -0500

    Architecture  Patterns With Python

--- Prog/Python/Architecture_Patterns_with_Python/test_batches.py
+++ Prog/Python/Architecture_Patterns_with_Python/test_batches.py
@@ -34,3 +34,9 @@ def test_can_only_deallocate_allocated_lines():
     batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
     batch.deallocate(unallocated_line)
     assert batch.available_quantity == 20
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18    
