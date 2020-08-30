d99fe7a471562fd23ca4ab18a2289f93c76db03a
Unimauo <unimauro@gmail.com>
2020-08-29 23:53:23 -0500

    Architecture  Patterns With Python

--- Prog/Python/Architecture_Patterns_with_Python/test_batches.py
+++ Prog/Python/Architecture_Patterns_with_Python/test_batches.py
@@ -20,3 +20,12 @@ def test_can_allocate_if_available_greater_than_required():
 def test_cannot_allocate_if_available_smaller_than_required():
     small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
     assert small_batch.can_allocate(large_line) is False
+
+def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+    assert batch.can_allocate(different_sku_line) is False    
