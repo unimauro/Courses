28443182ea8072a922d5d2f6f185a55d0475cc75
Unimauo <unimauro@gmail.com>
2020-08-30 18:34:16 -0500

    Architecture

--- Prog/Python/Architecture_Patterns_with_Python/OrderidSku.py
+++ Prog/Python/Architecture_Patterns_with_Python/OrderidSku.py
@@ -16,4 +16,17 @@ Line = namedtuple('Line', ['sku', 'qty'])
 def test_equality():
     assert Money('gbp', 10) == Money('gbp', 10)
     assert Name('Harry', 'Percival') != Name('Bob', 'Gregory')
-    assert Line('RED-CHAIR', 5) == Line('RED-CHAIR', 5)    
+    assert Line('RED-CHAIR', 5) == Line('RED-CHAIR', 5)   
+
+fiver = Money('gbp', 5)
+tenner = Money('gbp', 10)
+
+def can_add_money_values_for_the_same_currency():
+    assert fiver + fiver == tenner
+
+def can_subtract_money_values():
+    assert tenner - fiver == fiver
+
+def adding_different_currencies_fails():
+    with pytest.raises(ValueError):
+
