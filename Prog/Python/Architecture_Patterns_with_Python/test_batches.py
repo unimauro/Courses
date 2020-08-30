b76fdb4f8d31e3414c1914f628d608e26d473743
Unimauo <unimauro@gmail.com>
2020-08-29 23:52:54 -0500

    Architecture  Patterns With Python

--- Prog/Python/Architecture_Patterns_with_Python/test_batches.py
+++ Prog/Python/Architecture_Patterns_with_Python/test_batches.py
@@ -5,3 +5,18 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)
+    )
+
+
+def test_can_allocate_if_available_greater_than_required():
+    large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    assert large_batch.can_allocate(small_line)
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert small_batch.can_allocate(large_line) is False
