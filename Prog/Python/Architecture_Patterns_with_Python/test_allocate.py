058675a841a191548564e5dab27171193f143626
Unimauo <unimauro@gmail.com>
2020-08-30 22:57:26 -0500

    Architecture

--- Prog/Python/Architecture_Patterns_with_Python/test_allocate.py
+++ Prog/Python/Architecture_Patterns_with_Python/test_allocate.py
@@ -29,3 +29,11 @@ def test_returns_allocated_batch_ref():
     allocation = allocate(line, [in_stock_batch, shipment_batch])
     assert allocation == in_stock_batch.reference
 
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch('batch1', 'SMALL-FORK', 10, eta=today)
+    allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+
+    with pytest.raises(OutOfStock, match='SMALL-FORK'):
+        allocate(OrderLine('order2', 'SMALL-FORK', 1), [batch])
+
+
---
+++ Prog/Python/Architecture_Patterns_with_Python/xmodel.py
@@ -0,0 +1,25 @@
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    batch = next(
+        b for b in sorted(batches) if b.can_allocate(line)
+    )
+
+    batch.allocate(line)
+    return batch.reference
+
+
+class Batch:
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+    def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch('batch1', 'SMALL-FORK', 10, eta=today)
+    allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+
+    with pytest.raises(OutOfStock, match='SMALL-FORK'):
+        allocate(OrderLine('order2', 'SMALL-FORK', 1), [batch])
